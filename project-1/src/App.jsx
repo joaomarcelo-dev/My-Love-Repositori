19bb373ec562678154eb390f152d721a35420488
João Marcelo <joaomarcelolimaoliveira7@gmail.com>
2023-03-01 11:50:19 -0300

    Testando rotas em outro arquivo

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,6 +1,7 @@
 
-import './App.css'
-import Header from './components/Header/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './view/Home';
+import Coracao from './view/Coracao';
 
 function App() {
 
@@ -10,33 +11,12 @@ function App() {
 
   
     <div className='content-app'>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <h2>Feito com muito carinho para vc meu amor 🥰</h2>
-        <img className='gif-moh' src="https://media.tenor.com/KMNpADKiJwYAAAAC/heart-love.gif" alt="" />
-        <div className='text-one'>
-          <p>
-            Meu amor, vc é muito importante para mim 👩‍❤️‍💋‍👨. confesso para vc que nunca tinha sentido
-            tanto amor por outra pessoa assim como sinto por vc 😍... Vc é a luz que me ilumina ✨ a 
-            flor mais linda de todo o mundo 🌹❤️ !!! Sua delicadeza, suas loucuras ksksks, seus sorrisos, 
-            seu textos diários que vc me manda 🥰. tudo isso meu amor, contribuem para mim ser a 
-            pessoa que sou hoje 😌💞. hoje eu sou uma pessoa muito melhor graças a sua presença, graças 
-            a sua perseverança e motivação... Não consigo nem imaginar o quanto sou grato amor, por 
-            ter vc na minha vida 💍. Akele nosso primeiro encontro, já me mostrou que era vc a pessoa 
-            certa 😊. E eu sou grato por ele ter acontecido... sou grato por vc ter me mandado mensagem 
-            nakele dia que eu estava quase botando os bofes para fora ksksks 🤣🤣🤣, sou grato por vc aturar 
-            minhas crises... e mais ainda... Sou muito grato por ter vc em minha vida 🥹💕. 
-            <br /> <br />
-            EU TE AMO MUITO!!!!
-          </p>
-        </div>
-        
-        <a href='/coracao'>
-          Continue...
-        </a>
-      </main>
+      <Router>
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='/coracao' element={<Coracao />} />
+        </Routes>
+      </Router>
     </div>
     
   )
--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Coracao from './view/Coracao/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-        <Routes>
-          <Route index path='/' element={<App />} />
-          <Route path='/coracao' element={<Coracao />} />
-        </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 )
