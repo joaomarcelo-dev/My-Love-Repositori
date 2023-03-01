54358605f41e7398c1397e27d13ff1f7c9226fbd
João Marcelo <117828396+joaomarcelo-dev@users.noreply.github.com>
2023-03-01 11:21:01 -0300

    Update main.jsx

--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Coracao from './view/Coracao/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
         <Routes>
           <Route path='/' element={<App />} />
           <Route path='/coracao' element={<Coracao />} />
         </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 )
